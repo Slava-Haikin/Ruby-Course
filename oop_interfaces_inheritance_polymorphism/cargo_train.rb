8363dc91737b24e5ac10c8ff8f5c359902988f00
Slava Haikin <vyachowski@gmail.com>
2025-01-29 19:51:51 +0700

    feat: add separate classes for trains

--- oop_interfaces_inheritance_polymorphism/cargo_train.rb
+++ oop_interfaces_inheritance_polymorphism/cargo_train.rb
@@ -1,8 +1,8 @@
 # Разделить поезда на два типа PassengerTrain и CargoTrain, сделать родителя для классов,
 # который будет содержать общие методы и свойства
 
-class PassengertTrain < Train
-  def initialize(train_number)
-    super(train_number, 'passenger')
-  end
+class CargoTrain < Train
+    def initialize(train_number)
+      super(train_number, 'passenger')
+    end
 end
--- oop_interfaces_inheritance_polymorphism/freight_train.rb
+++
@@ -1,3 +0,0 @@
-class FreightTrain < Train
-
-end
