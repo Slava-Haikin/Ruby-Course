7ad8af551f7a50e1dd8b13a16c436d9483781d16
Slava Haikin <vyachowski@gmail.com>
2025-02-02 22:30:38 +0700

    refactor: simplify app logic

--- oop_interfaces_inheritance_polymorphism/main.rb
+++ oop_interfaces_inheritance_polymorphism/main.rb
@@ -23,19 +23,19 @@ class App
     @routes = []
     @trains = []
     @stations = []
+    @initialized = false
   end
 
   def start()
-    first_run = true
     loop do
-      unless first_run
+      if @initialized
         get_user_input 'To continue press "Enter"'
       end
 
       show_menu
-      signal = get_user_input 'Enter your command:'
-      execute_menu_command(signal)
-      first_run = false
+      command = get_user_input 'Enter your command:'
+      execute_menu_command(command)
+      @initialized = false
     end
   end
 
@@ -58,89 +58,19 @@ class App
     '
   end
 
-  def get_user_input(message)
-    puts message
+  def get_user_input(message = nil)
+    puts message if message
     gets.chomp
   end
 
-  def execute_menu_command(signal)
-    case signal
+  def execute_menu_command(command)
+    case command
     when '1', 's', 'create station' then create_station
     when '2', 't', 'create train' then create_train
-    when '3', 'r', 'create route'
-      puts 'Choose starting station number and press enter'
-      print_indexed_list(@stations)
-      starting_station_input = gets.chomp
-      starting_station = @stations[starting_station_input.to_i]
-
-      puts 'Choose final station number and press enter'
-      print_indexed_list(@stations)
-      final_station_input = gets.chomp
-      final_station = @stations[final_station_input.to_i]
-
-      create_route(starting_station, final_station) if starting_station && final_station
-
-      print_indexed_list(@routes)
-
-      start
-    when '4', 'y', 'modify route'
-      puts 'Choose route to modify'
-
-      print_indexed_list(@routes)
-
-      route_index = gets.chomp.to_i
-      modifying_route = @routes[route_index]
-
-      puts 'Do you want to delete station or to add? Type add or delete'
-
-      action_type = gets.chomp
-
-      if action_type == 'add'
-        puts 'Choose station to add'
-
-        available_stations = @stations - modifying_route.get_stations_list
-
-        print_indexed_list(available_stations)
-
-        adding_station_index = gets.chomp.to_i
-        adding_station = available_stations[adding_station_index]
-
-        if adding_station
-          modifying_route.add_station(adding_station)
-          puts "Station #{adding_station.name} added successfully."
-        else
-          puts "Invalid selection."
-        end
-
-      elsif action_type == 'delete'
-        puts 'Choose station to delete (cannot remove first or last station):'
-
-        removable_stations = modifying_route.get_stations_list[1..-2]
-
-        if removable_stations.empty?
-          puts "No stations available for removal."
-        else
-          print_indexed_list(removable_stations)
-
-          deleting_station_index = gets.chomp.to_i
-          deleting_station = removable_stations[deleting_station_index]
-
-          if deleting_station
-            modifying_route.remove_station(deleting_station)
-            puts "Station #{deleting_station.name} removed successfully."
-          else
-            puts "Invalid selection."
-          end
-        end
-      else
-        puts "Unknown action."
-      end
-
-      print_indexed_list(modifying_route.get_stations_list)
-      start
-    when '5', 'y', 'set route'
-
-    when '6', 'y', 'attach wagon'
+    when '3', 'r', 'create route' then create_route
+    when '4', 'y', 'modify route' then modify_route
+    when '5', 'y', 'set route' then set_train_route
+    when '6', 'y', 'attach wagon' then attach_wagon
     when '7', 'y', 'detach wagon'
     when '8', 'm', 'move train'
     when '9', 'l', 'stations list'
@@ -154,7 +84,7 @@ class App
 
     @stations << Station.new(station_name.empty? ? random_station_name : station_name)
 
-    print_indexed_list(@stations, 'New stations list:')
+    print_list(@stations, 'New stations list:')
   end
 
   def create_train()
@@ -164,12 +94,90 @@ class App
     newTrain = train_type == 'cargo' ? CargoTrain.new(train_number) : PassengerTrain.new(train_number)
     @trains << newTrain
 
-    print_indexed_list(@trains, 'New train list:')
+    print_list(@trains, 'New train list:')
   end
 
-  def create_route(starting_station, final_station)
+  def create_route()
+    print_list(@stations)
+    starting_station_index = get_user_input('Choose starting station number and press enter').to_i
+    starting_station = @stations[starting_station_index]
+
+    print_list(@stations - [starting_station])
+    final_station_index = get_user_input('Choose final station number and press enter').to_i
+
+    final_station = @stations[final_station_index]
+
+    raise 'Route was not created, please try again' unless starting_station && final_station
+
     newRoute = Route.new(starting_station, final_station)
     @routes << newRoute
+    print_list(@routes)
+  end
+
+  def modify_route()
+    print_list(@routes)
+
+    route_index_input = get_user_input('Choose route number to modify:')
+    modifying_route = @routes[route_index_input.to_i]
+
+    action_type = get_user_input 'Do you want add or delete station?'
+    case action_type
+    when 'add'
+      available_stations = @stations - modifying_route.get_stations_list
+
+      print_list(available_stations, 'Choose station to add')
+
+      adding_station_index_input = get_user_input
+      adding_station = available_stations[adding_station_index_input.to_i]
+
+      if adding_station
+        modifying_route.add_station(adding_station)
+        puts "Station #{adding_station.name} added successfully."
+      else
+        puts "Invalid selection."
+      end
+    when 'delete'
+      puts 'Choose station to delete (cannot remove first or last station):'
+
+      removable_stations = modifying_route.get_stations_list[1..-2]
+
+      if removable_stations.empty?
+        puts "No stations available for removal."
+      else
+        print_list(removable_stations)
+
+        deleting_station_index_input = get_user_input
+        deleting_station = removable_stations[deleting_station_index_input.to_i]
+
+        if deleting_station
+          modifying_route.remove_station(deleting_station)
+          puts "Station #{deleting_station.name} removed successfully."
+        else
+          puts "Invalid selection."
+        end
+      end
+    else
+      puts "Unknown action."
+    end
+
+    print_list(modifying_route.get_stations_list)
+  end
+
+  def set_train_route()
+    print_list(@trains, 'Trains:')
+    modifying_train_index = get_user_input 'Choose a train number to set up the route:'.to_i
+    modifying_train = @trains[modifying_train_index]
+
+    raise 'Not an existing train' unless modifying train
+
+    print_list(@routes, 'Routes:')
+    adding_route_index = get_user_input 'Choose a route to add:'.to_i
+    adding_route = @trains[adding_route_index]
+
+    raise 'Not an existing route' unless modifying train
+
+    modifying_train.set_route(adding_route)
+    puts 'Route was added!'
   end
 
   def random_station_name
@@ -186,7 +194,7 @@ class App
     "#{prefix} #{suffix}"
   end
 
-  def print_indexed_list(array, message)
+  def print_list(array, message = nil)
     if (message)
       puts message
     end
