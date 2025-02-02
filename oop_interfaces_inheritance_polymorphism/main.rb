8863623ae346c90abefbd4afcec0936dd5a8b732
Slava Haikin <vyachowski@gmail.com>
2025-02-02 22:42:48 +0700

    feat: complete train app logic

--- oop_interfaces_inheritance_polymorphism/main.rb
+++ oop_interfaces_inheritance_polymorphism/main.rb
@@ -71,10 +71,10 @@ class App
     when '4', 'y', 'modify route' then modify_route
     when '5', 'y', 'set route' then set_train_route
     when '6', 'y', 'attach wagon' then attach_wagon
-    when '7', 'y', 'detach wagon'
-    when '8', 'm', 'move train'
-    when '9', 'l', 'stations list'
-    when '10', 'o', 'trains list'
+    when '7', 'y', 'detach wagon' then detach_wagon
+    when '8', 'm', 'move train' then move_train
+    when '9', 'l', 'stations list' then stations_list
+    when '10', 'o', 'trains list' then trains_list
     when '0', 'E' then exit
     end
   end
@@ -107,7 +107,10 @@ class App
 
     final_station = @stations[final_station_index]
 
-    raise 'Route was not created, please try again' unless starting_station && final_station
+    if starting_station.nil? || final_station.nil?
+      puts 'Route was not created, please try again.'
+      return
+    end
 
     newRoute = Route.new(starting_station, final_station)
     @routes << newRoute
@@ -117,8 +120,8 @@ class App
   def modify_route()
     print_list(@routes)
 
-    route_index_input = get_user_input('Choose route number to modify:')
-    modifying_route = @routes[route_index_input.to_i]
+    route_index = get_user_input('Choose route number to modify:').to_i
+    modifying_route = @routes[route_index]
 
     action_type = get_user_input 'Do you want add or delete station?'
     case action_type
@@ -165,19 +168,100 @@ class App
 
   def set_train_route()
     print_list(@trains, 'Trains:')
-    modifying_train_index = get_user_input 'Choose a train number to set up the route:'.to_i
+    modifying_train_index = get_user_input('Choose a train number to set up the route:').to_i
     modifying_train = @trains[modifying_train_index]
 
-    raise 'Not an existing train' unless modifying train
+    unless modifying_train
+      puts 'Not an existing train'
+      return
+    end
 
     print_list(@routes, 'Routes:')
-    adding_route_index = get_user_input 'Choose a route to add:'.to_i
+    adding_route_index = get_user_input('Choose a route to add:').to_i
     adding_route = @trains[adding_route_index]
 
-    raise 'Not an existing route' unless modifying train
+    unless adding_route
+      puts 'Not an existing route'
+      return
+    end
 
     modifying_train.set_route(adding_route)
     puts 'Route was added!'
+  end
+
+  def attach_wagon
+    print_list(@trains, 'Choose a train to attach a wagon:')
+    train_index = get_user_input.to_i
+    train = @trains[train_index]
+
+    unless train
+      puts 'Invalid train selection'
+      return
+    end
+
+    wagon = train.type == :cargo ? CargoWagon.new : PassengerWagon.new
+    train.attach_wagon(wagon)
+
+    puts "Wagon successfully attached to train #{train.number}"
+  end
+
+  def detach_wagon
+    print_list(@trains, 'Choose a train to detach a wagon:')
+    train_index = get_user_input.to_i
+    train = @trains[train_index]
+
+    unless train
+      puts 'Invalid train selection'
+      return
+    end
+
+    if train.wagons.empty?
+      puts 'No wagons to detach.'
+    else
+      train.detach_wagon
+      puts "Wagon successfully detached from train #{train.number}"
+    end
+  end
+
+  def move_train
+    print_list(@trains, 'Choose a train to move:')
+    train_index = get_user_input.to_i
+    train = @trains[train_index]
+
+    unless train
+      puts 'Invalid train selection'
+      return
+    end
+
+    direction = get_user_input('Enter direction (forward/backward):')
+
+    case direction
+    when 'forward'
+      train.move_forward
+      puts "Train #{train.number} moved forward."
+    when 'backward'
+      train.move_backward
+      puts "Train #{train.number} moved backward."
+    else
+      puts 'Invalid direction.'
+    end
+  end
+
+  def stations_list
+    print_list(@stations, 'List of stations:')
+  end
+
+  def trains_list
+    print_list(@stations, 'Choose a station:')
+    station_index = get_user_input.to_i
+    station = @stations[station_index]
+
+    unless station
+      puts 'Invalid station selection'
+      return
+    end
+
+    print_list(station.trains, "Trains at #{station.name}:")
   end
 
   def random_station_name
