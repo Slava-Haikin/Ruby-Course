350fde338c96e7a913d5d2b1db9ed5f5d7830446
Slava Haikin <vyachowski@gmail.com>
2025-02-28 13:47:16 +0700

    feat: add cargo wagon part

--- 5.Lambdas_and_Procs/cargo_wagon.rb
+++ 5.Lambdas_and_Procs/cargo_wagon.rb
@@ -7,11 +7,28 @@
 =end
 
 class CargoWagon < Wagon
+  attr_reader :total_volume, :occupied_volume, :measure_unit
+
   def initialize
     super('cargo')
 
     @total_volume = 138
+    @occupied_volume = 0
+    @measure_unit = 'm3'
   end
 
-  def total_volume = "#{@total_volume}m3"
+  def print_total_volume = print "#{@total_volume}#{@measure_unit}"
+  def available_volume = @total_volume - @occupied_volume
+
+  def load_cargo(volume)
+    return if available_volume < volume
+
+    @occupied_volume += volume
+  end
+
+  def unload_cargo(volume)
+    return if @occupied_volume < volume
+
+    @occupied_volume -= volume
+  end
 end
