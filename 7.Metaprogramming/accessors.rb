ced024cf325f45990e5007c8709771baeac83c33
Slava Haikin <vyachowski@gmail.com>
2025-03-08 03:14:36 +0700

    feat: add history methods and tests

--- 7.Metaprogramming/accessors.rb
+++ 7.Metaprogramming/accessors.rb
@@ -1,22 +1,23 @@
 # frozen_string_literal: true
+
 # Accessors module provides a set of methods to mix in dynamic methods into other classes
 
 # Написать модуль Acсessors, содержащий следующие методы, которые можно вызывать на уровне класса:
 #  Метод attr_accessor_with_history:
-#  - Этот метод динамически создает геттеры и сеттеры для любого кол-ва атрибутов, при этом сеттер сохраняет все значения 
-#    инстанс-переменной при изменении этого значения. 
-#  - Также в класс, в который подключается модуль должен добавляться инстанс-метод <имя_атрибута>_history который возвращает 
-#    массив всех значений данной переменной.
+#  - Этот метод динамически создает геттеры и сеттеры для любого кол-ва атрибутов, при этом сеттер
+#    сохраняет все значения
+#    инстанс-переменной при изменении этого значения.
+#  - Также в класс, в который подключается модуль должен добавляться инстанс-метод <имя_атрибута>_history,
+#    который возвращает массив всех значений данной переменной.
 #  Метод strong_attr_accessor:
-#  - Который принимает имя атрибута и его класс. При этом создается геттер и сеттер для одноименной инстанс-переменной, 
-#    но сеттер проверяет тип присваемоего значения. Если тип отличается от того, который указан вторым параметром, то выбрасывается исключение. Если тип совпадает, то значение присваивается.
+#  - Который принимает имя атрибута и его класс. При этом создается геттер и сеттер для одноименной инстанс-переменной,
+#    но сеттер проверяет тип присваемоего значения. Если тип отличается от того, который указан вторым параметром,
+#    то выбрасывается исключение. Если тип совпадает, то значение присваивается.
 
-module Acсessors
+module Accessors
   def attr_accessor_with_history(*args)
     args.each do |arg|
-      define_method(arg) do
-        instance_variable_get("@#{arg}")
-      end
+      define_method(arg) { instance_variable_get("@#{arg}") }
 
       define_method("#{arg}=") do |value|
         old_value = instance_variable_get("@#{arg}")
@@ -27,13 +28,11 @@ module Acсessors
         instance_variable_set("@#{arg}", value)
       end
 
-      define_method("#{arg}_history") do
-        instance_variable_get("@#{arg}_history") || []
-      end
+      define_method("#{arg}_history") { instance_variable_get("@#{arg}_history") || [] }
     end
   end
 
-  def strong_attr_accessor(attr_name, class)
+  def strong_attr_accessor(_attr_name, _target_class)
     puts 'Hello, World!'
   end
 end
--- 7.Metaprogramming/accessors_test.rb
+++ 7.Metaprogramming/accessors_test.rb
@@ -1,12 +1,40 @@
+# frozen_string_literal: true
+
+# Test class for Accessors
 require_relative 'accessors'
 
 class AccessorsTest
-  include Accessors
+  extend Accessors
+  attr_accessor_with_history(:test, :another_test, :final_test)
 end
 
-AccessorsTest.attr_accessor_with_history(:test :another_test :final_test)
 test = AccessorsTest.new
 
+# Should be empty
 puts test.test
 puts test.another_test
 puts test.final_test
+
+# Assign values
+test.test = 'test'
+test.another_test = 'another_test'
+test.final_test = 'final_test'
+
+# Should be equal to assigned values
+puts test.test
+puts test.another_test
+puts test.final_test
+
+# Re-assign values
+test.test = 'test_1'
+test.another_test = 'another_test_1'
+test.final_test = 'final_test_1'
+
+test.test = 'test_2'
+test.another_test = 'another_test_2'
+test.final_test = 'final_test_2'
+
+# Each history should have two records
+print test.test_history          # => ["test", "test_1"]
+print test.another_test_history  # => ["another_test", "another_test_1"]
+print test.final_test_history    # => ["final_test", "final_test_1"]
